decc0120c52e72099be144e3fc3cc0a15a4e27a4
Tyler Paulson <me@tylerpaulson.com>
2015-01-15 17:59:54 -0500

    Use HTML break tags when loading TXT template

--- includes/functions.php
+++ includes/functions.php
@@ -2,23 +2,20 @@
 
 function write_email($data, $html = false) {
 
-	if($html) {
-		$break = '<br>';
-	} else {
-		$break = "\n";
-	}
-
 	$template_file = 'template/sample.txt';
 
 	if (!file_exists($template_file)) {
 	    return 'Template file not found!';
-	    exit;
 	}
 
 	$msg_template = file_get_contents($template_file);
 	$array_template_vars = array('{username}', '{password}', '{firstname}', '{lastname}');
 	$array_template_data = array($data[0], $data[1], $data[2], $data[3]);
 	$final_msg = str_replace($array_template_vars, $array_template_data, $msg_template);
+	
+	if($html) {
+		$final_msg = str_replace("\n", "<br>", $final_msg);
+	}
 
 	return $final_msg;
 
