5ef792ccf1947e3a25e1f388d6e7984965cc523d
Tyler Paulson <me@tylerpaulson.com>
2015-02-13 14:52:20 -0500

    Fixing sort JS

--- scripts.js
+++ scripts.js
@@ -2,9 +2,15 @@
 	
 	var template_options = $('#template option');
 	template_options.sort(function(a,b){
-		a = a.value;
-		b = b.value;
-		return a-b;
+		if (a.value > b.value) {
+			return 1;
+		}
+		else if (a.value < b.value) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
 	});
 	$('#template').html(template_options);
 	
